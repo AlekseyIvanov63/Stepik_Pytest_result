a0870bc6c72de0a9ad1aa419dbd4a847df8bd097
AlekseyIvanov63 <163aleksey2023@gmail.com>
2024-03-22 20:50:45 +0400

    add test 'adding to cart from product page'

--- pages/product_page.py
+++ pages/product_page.py
@@ -1,5 +1,7 @@
 from pages.base_page import BasePage
 from pages.locators import ProductPageLocators
+from selenium.common.exceptions import NoAlertPresentException
+import math
 
 
 class ProductPage(BasePage):
@@ -13,3 +15,21 @@ class ProductPage(BasePage):
     def should_be_add_to_basket(self):
         assert self.is_element_present(*ProductPageLocators.ADD_TO_BASKET_BUTTON), \
             "Button add to basket is not presented"
+
+    def add_product_to_basket(self):
+        add_to_basket = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET_BUTTON)
+        add_to_basket.click()
+
+    def solve_quiz_and_get_code(self):
+        alert = self.browser.switch_to.alert
+        x = alert.text.split(" ")[2]
+        answer = str(math.log(abs((12 * math.sin(float(x))))))
+        alert.send_keys(answer)
+        alert.accept()
+        try:
+            alert = self.browser.switch_to.alert
+            alert_text = alert.text
+            print(f"Your code: {alert_text}")
+            alert.accept()
+        except NoAlertPresentException:
+            print("No second alert presented")
--- test_product_page.py
+++ test_product_page.py
@@ -8,3 +8,16 @@ def test_guest_should_see_product_link(browser):
     page = ProductPage(browser, link)
     page.open()
     page.should_be_product_url()
+
+
+def test_guest_should_add_to_basket(browser):
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_add_to_basket()
+
+
+def test_guest_can_add_product_to_basket(browser):
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_product_to_basket()
+    page.solve_quiz_and_get_code()
